117d37ac673b9bf58d96edfd1bf3aed245c4f7d1
AVikharev <rJfQG58bWv>
2021-03-24 15:17:03 +0500

    Task 2,3

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -1,7 +1,20 @@
 package com.max.idea;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        double b = 3.1416;
+        byte с = 127;
+
+        int [] arrofint = {0,1,2,3,4};
+        int temp =  arrofint[0];
+        arrofint[0]=arrofint[arrofint.length-1];
+        arrofint[arrofint.length-1]=temp;
+        System.out.println(Arrays.deepToString(new int[][]{arrofint}));
+
+        int sumFirstAndAvgEl = arrofint[0] + arrofint[arrofint.length/2];
+        System.out.println(sumFirstAndAvgEl);
     }
 }
